dcf48e2803660a438ac745a139609ad2f3ae6eb9
okr765 <me@okr765.com>
2025-08-17 21:18:45 -0400

    Add meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,18 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>Cigarette Client</title>
+	<meta
+		name="description"
+		content="A next-generation Minecraft hacked client for Hypixel minigames. Completely free and open source."
+	/>
+	<meta name="og:title" content="Cigarette Client" />
+	<meta name="og:url" content="https://cigarette.dev" />
+	<meta
+		name="og:description"
+		content="A next-generation Minecraft hacked client for Hypixel minigames. Completely free and open source."
+	/>
+	<meta name="og:image" content={favicon} />
+	<link rel="canonical" href="https://cigarette.dev" />
 </svelte:head>
 
 <div class="font-os fixed z-10 h-full w-full overflow-auto bg-slate-950 text-white">
